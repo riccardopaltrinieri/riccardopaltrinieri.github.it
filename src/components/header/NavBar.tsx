c6dde3a85a167252bfda0cd79899c32dbea5610a
Riccardo Paltrinieri <rpaltrinieri@bunq.com>
2022-09-22 20:47:26 +0200

    Removed Dropdown

--- src/components/header/NavBar.tsx
+++ src/components/header/NavBar.tsx
@@ -1,5 +1,5 @@
 import {Component} from "react";
-import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
+import {Container, Nav, Navbar} from "react-bootstrap";
 import {Link} from "react-router-dom";
 
 /**
@@ -11,17 +11,12 @@ class NavBar extends Component<NavBarProps> {
         return <div className="nav-bar">
             <Navbar bg="black" variant="dark" expand="lg" className="py-2 fs-5">
                 <Container>
-                    <Navbar.Brand href="/" className="fs-3">Portfolio</Navbar.Brand>
+                    <Navbar.Brand href="/"><h1 className="fs-3">Portfolio</h1></Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav" role="tablist">
                         <Nav className="me-auto">
-                            <NavDropdown title="About" id="basic-nav-dropdown" menuVariant="dark" className="navbar-dropdown">
-                                <NavDropdown.Item as={Link} to="/education">Education</NavDropdown.Item>
-                                <NavDropdown.Item as={Link} to="/project">Project</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item as={Link} to="/#something-about-me">Something about me</NavDropdown.Item>
-                            </NavDropdown>
                             <Nav.Link as={Link} to="/work">Work</Nav.Link>
+                            <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
                             <Nav.Link as={Link} to="/resume">Resume</Nav.Link>
                             <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
                         </Nav>
