b05f16dfa1a9b8b4f9f63255a544f7b0fe41f672
Riccardo Paltrinieri <riccardo@underpaid.space>
2024-06-08 23:47:53 +0200

    Fix navbar that doesnt collapse after navigating

--- src/components/header/NavBar.tsx
+++ src/components/header/NavBar.tsx
@@ -1,5 +1,6 @@
-import {Container, Nav, Navbar} from "react-bootstrap";
-import {Link} from "react-router-dom";
+import { useState } from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import './index.css';
 
 /**
@@ -7,22 +8,32 @@ import './index.css';
  * @since 20220606 Initial creation.
  */
 export const NavBar = () => {
-    return <div className="rpal-navbar">
-        <Navbar bg="black" variant="dark" expand="lg" className="py-2 fs-5">
-            <Container>
+    const [expanded, setExpanded] = useState(false);
+
+    const handleToggle = () => {
+        setExpanded(!expanded);
+    };
+
+    const handleLinkClick = () => {
+        setExpanded(false);
+    };
+
+    return (
+        <Container>
+            <Navbar bg="black" variant="dark" expand="lg" className="py-2 fs-5" expanded={expanded}>
                 <Navbar.Brand href="/"><h1 className="fs-3">Portfolio</h1></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggle}/>
                 <Navbar.Collapse id="basic-navbar-nav" role="tablist">
                     <Nav className="me-auto">
-                        <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
-                        <Nav.Link as={Link} to="/resume">Resume</Nav.Link>
+                        <Nav.Link as={Link} to="/projects" onClick={handleLinkClick}>Projects</Nav.Link>
+                        <Nav.Link as={Link} to="/resume" onClick={handleLinkClick}>Resume</Nav.Link>
                     </Nav>
                     <Nav>
-                        <Nav.Link as={Link} to="/about">About</Nav.Link>
-                        <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+                        <Nav.Link as={Link} to="/about" onClick={handleLinkClick}>About</Nav.Link>
+                        <Nav.Link as={Link} to="/contact" onClick={handleLinkClick}>Contact</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    </div>;
-}
+            </Navbar>
+        </Container>
+    );
+};
